947e7d971664d35d416eb0cc614d362afb4c06ef
Zaka Salah Eddine <zakasalaheddine@gmail.com>
2021-02-05 15:59:10 +0100

    update window.onload with jquery ready function

--- search-schools/studen-loan-controls.js
+++ search-schools/studen-loan-controls.js
@@ -1,15 +1,15 @@
-let colleges = [];
-const zoneAttributes = {};
+let colleges = []
+const zoneAttributes = {}
 
 function loadColleges() {
   fetch(
-    "https://cdn.jsdelivr.net/gh/salaheddinezaka/lincx-controls/search-schools/schools.json"
-  ).then((res) => res.json().then((vals) => (colleges = vals)));
+    'https://cdn.jsdelivr.net/gh/salaheddinezaka/lincx-controls/search-schools/schools.json'
+  ).then((res) => res.json().then((vals) => (colleges = vals)))
 }
 
 function insertModalIntoPage() {
-  const modalElement = document.createElement("div");
-  modalElement.id = "modal";
+  const modalElement = document.createElement('div')
+  modalElement.id = 'modal'
   modalElement.innerHTML = `
   <div class="scores__modal--background"></div>
   <div class="scores__modal" style="display: block; top: 50px; opacity: 1;">
@@ -44,18 +44,18 @@ function insertModalIntoPage() {
         <button class="loan__button" onclick="">FIND LOANS</button>
       </div>
     </div>
-  </div>`;
-  document.body.appendChild(modalElement);
+  </div>`
+  document.body.appendChild(modalElement)
 }
 
 function hideModal() {
-  $(".scores__modal--background").first().hide();
-  $(".scores__modal").first().hide();
+  $('.scores__modal--background').first().hide()
+  $('.scores__modal').first().hide()
 }
 
 function showModal() {
-  $(".scores__modal--background").first().show();
-  $(".scores__modal").first().slideDown();
+  $('.scores__modal--background').first().show()
+  $('.scores__modal').first().slideDown()
 }
 
 function addAutoComplete(element, callbackOnSubmit) {
@@ -63,15 +63,15 @@ function addAutoComplete(element, callbackOnSubmit) {
   new Autocomplete(element, {
     search: (input) => {
       if (input.length < 1) {
-        return [];
+        return []
       }
       return colleges.filter((college) => {
-        return college.name.toLowerCase().startsWith(input.toLowerCase());
-      });
+        return college.name.toLowerCase().startsWith(input.toLowerCase())
+      })
     },
     onSubmit: (result) => {
-      console.log(result);
-      callbackOnSubmit(result);
+      console.log(result)
+      callbackOnSubmit(result)
     },
     getResultValue: (result) => result.name,
     renderResult: (result, props) => `
@@ -82,26 +82,26 @@ function addAutoComplete(element, callbackOnSubmit) {
       <span class="college__city">${result.state.name} ${result.state.region.name}</span>
     </div>
     `
-  });
+  })
 }
 
 function updateZoneAttribute(attribute, value) {
-  zoneAttributes[attribute] = value;
-  console.log(zoneAttributes);
+  zoneAttributes[attribute] = value
+  console.log(zoneAttributes)
   // window.renderAdFeed(zoneAttributes);
 }
 
 function handleModalSubmit(value) {
-  updateZoneAttribute("data-school", value);
-  hideModal();
+  updateZoneAttribute('data-school', value)
+  hideModal()
 }
 
 function handleSearchSubmit(value) {
-  updateZoneAttribute("data-school", value);
+  updateZoneAttribute('data-school', value)
 }
 
 function createSearchInput() {
-  document.getElementById("search-schools-control").innerHTML = `
+  document.getElementById('search-schools-control').innerHTML = `
     <div class="search__control--container">
     <h3>Which college will you be attending?</h3>
     <div class="control__form">
@@ -116,49 +116,48 @@ function createSearchInput() {
       <button class="search__button">FIND LOANS</button>
     </div>
   </div>
-  `;
+  `
 }
 
 function makeSearchFixedOnScroll() {
-  const searchContainerTop = document.getElementById("search-schools-control")
-    .offsetTop;
+  const searchContainerTop = document.getElementById('search-schools-control')
+    .offsetTop
   $(window).scroll(() => {
-    const currentScroll = $(window).scrollTop();
+    const currentScroll = $(window).scrollTop()
     if (currentScroll >= searchContainerTop) {
-      $("#search-schools-control").css({
-        position: "fixed",
-        top: "0",
-        left: "0",
-        width: "100%",
-        "z-index": 1
-      });
+      $('#search-schools-control').css({
+        position: 'fixed',
+        top: '0',
+        left: '0',
+        width: '100%',
+        'z-index': 1
+      })
     } else {
-      $("#search-schools-control").css({
-        position: "static"
-      });
+      $('#search-schools-control').css({
+        position: 'static'
+      })
     }
-  });
+  })
 }
 
 function waitFor(element, callback) {
   const checkExist = setInterval(function () {
     if (document.querySelector(element)) {
-      callback();
-      clearInterval(checkExist);
+      callback()
+      clearInterval(checkExist)
     }
-  }, 100);
+  }, 100)
 }
-window.onload = function () {
-  if (window.jQuery) {
-    insertModalIntoPage();
-    hideModal();
-    waitFor("#search-schools-control", async () => {
-      createSearchInput();
-      loadColleges();
-      showModal();
-      addAutoComplete("#autocomplete", handleModalSubmit);
-      addAutoComplete("#autocomplete__input", handleSearchSubmit);
-      makeSearchFixedOnScroll();
-    });
-  }
-};
+
+$(document).ready(function () {
+  insertModalIntoPage()
+  hideModal()
+  waitFor('#search-schools-control', async () => {
+    createSearchInput()
+    loadColleges()
+    showModal()
+    addAutoComplete('#autocomplete', handleModalSubmit)
+    addAutoComplete('#autocomplete__input', handleSearchSubmit)
+    makeSearchFixedOnScroll()
+  })
+})
